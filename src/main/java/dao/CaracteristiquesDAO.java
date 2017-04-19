1c1e4b071df0dbdb0e6f64c4f82ae2857be36beb
Rafik Zebdi <rzi@acial.loc>
2017-04-19 16:00:07 +0200

    Fix REST @delete

--- src/main/java/dao/CaracteristiquesDAO.java
+++ src/main/java/dao/CaracteristiquesDAO.java
@@ -32,11 +32,15 @@ public class CaracteristiquesDAO {
     }
 
     public Caracteristique recupererUneCaracteristique(final String denominationATrouver) throws NoResultException{
+
         final String JPQLQuery = "SELECT c FROM Caracteristique c WHERE c.denomination LIKE :denomination";
+
         final TypedQuery<Caracteristique> query = em.createQuery(JPQLQuery, Caracteristique.class);
+
         query.setParameter("denomination", denominationATrouver);
 
         final Caracteristique caracteristique = query.getSingleResult();
+
         if (caracteristique == null){
             throw new NoResultException();
         }
